288b4b0f703d8f6d32685d8acb6db21bf8c1cc6c
Nikita <nik.zav.2005@gmail.com>
2024-03-09 12:50:28 +0300

    Update dst

--- DST.cpp
+++ DST.cpp
@@ -13,7 +13,7 @@ private:
     std::vector<int64_t> pref, suf;
 
     int64_t sum(size_t i, size_t j) {
-      return pref[i] + suf[j];
+      return suf[i] + pref[j];
     }
   };
 
@@ -21,12 +21,10 @@ private:
     for (size_t i = 0; i < std::__lg(vec.size()); i++) {
       size_t p = 1LL << i;
       for (size_t k = 0; k < (vec.size() >> (i + 1)); k++) {
-        precalc tmp(1LL << i);
-        tmp.pref[0] = vec[p];
-        tmp.suf[0] = vec[p - 1];
-        for (size_t j = 1; j < i; j++) {
-          tmp.pref[j] = tmp.pref[j - 1] + vec[p + j];
-          tmp.suf[j] = tmp.suf[j - 1] + vec[p - 1 - j];
+        precalc tmp((1LL << i) + 1);
+        for (size_t j = 1; j < (1LL << i) + 1; j++) {
+          tmp.pref[j] = tmp.pref[j - 1] + vec[p + j - 1];
+          tmp.suf[j] = tmp.suf[j - 1] + vec[p - j];
         }
         table[p] = tmp;
         p += 1LL << (i + 1);
@@ -43,6 +41,6 @@ public:
 
   int64_t sum(size_t l, size_t r) {
     std::size_t m = (r >> std::__lg(l ^ r)) << std::__lg(l ^ r);
-    return table[m].sum(m - l - 1, r - m);
+    return table[m].sum(m - l, r - m);
   }
 };
