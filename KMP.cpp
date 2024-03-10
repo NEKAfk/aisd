36affdf74c5846ec0f4a7a42c70f746ec4a70b11
Nikita <nik.zav.2005@gmail.com>
2024-03-10 10:39:02 +0300

    Add KMP

---
+++ KMP.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+inline std::size_t prefCount(std::size_t init, std::string& temp, char c, std::vector<size_t>& pref) {
+  std::size_t k = init;
+  while (k > 0 && c != temp[k]) {
+    k = pref[k - 1];
+  }
+  if (c == temp[k]) {
+    k++;
+  }
+  return k;
+}
+
+std::vector<std::size_t> KMP(std::string& P, std::string& Q) {
+  std::vector<std::size_t> pref(P.size(), 0);
+  for (size_t i = 1; i < P.size(); i++) {
+    pref[i] = prefCount(pref[i - 1], P, P[i], pref);
+  }
+
+  std::vector<std::size_t> result;
+  std::size_t last = 0;
+  for (std::size_t i = 0; i < Q.size(); i++) {
+    last = prefCount(last, P, Q[i], pref);
+    if (last == P.size()) {
+      result.push_back(i - P.size() + 1);
+    }
+  }
+  return result;
+}
